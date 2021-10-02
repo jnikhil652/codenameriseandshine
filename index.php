b331bda502de985edb21bd732f063d6072550943
Nikhil Jadhav <jnikhil652@gmail.com>
2021-10-02 22:16:21 +0530

    set timeout

--- index.php
+++ index.php
@@ -809,6 +809,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js"></script>
     <script>
         $(document).ready(function() {
+            setTimeout(function() {
+                popup();
+            }, 5000);
             setInterval(function() {
                 popup();
             }, 30000);
